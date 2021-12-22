e47a4ccd7ade94750c829ebcd9dbe18c98eb897b
lsk15602329916 <lsk15602329916@163.com>
2021-12-23 00:59:29 +0800

    合并代码

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,20 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import NewsList from '../views/NewsList.vue'
 
-const routes = [
-  {
-    path: '/',
-    component: NewsList,
-  },
-  {
-    path: '/:tag',
-    name: 'NewsList',
-    component: NewsList,
-  },
+const routes = [{
+        path: '/',
+        component: NewsList,
+    },
+    {
+        path: '/:tag',
+        name: 'NewsList',
+        component: NewsList,
+    },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,25 @@
+// const path = require("path")
+// const resolve = dir => path.join(_dirname, dir)
+
 module.exports = {
-  pluginOptions: {
-    vuetify: {
-      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-    }
-  },
-  devServer: {
-    /* devServer.proxy: (跨域问题)如果前端应用和后端api服务器没有运行在同一主机上
-        则需要在开发环境下将API请求代理到api服务器 */
-    proxy: {
-      // 配置跨域
-      '/toutiao': {
-        target: 'https://m.toutiao.com/',
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/toutiao': ''
+    pluginOptions: {
+        vuetify: {
+            // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
         }
-      },
+    },
+    devServer: {
+        /* devServer.proxy: (跨域问题)如果前端应用和后端api服务器没有运行在同一主机上
+            则需要在开发环境下将API请求代理到api服务器 */
+        proxy: {
+            // 配置跨域
+            '/toutiao': {
+                target: 'https://m.toutiao.com/',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/toutiao': ''
+                }
+            },
+        }
     }
-  }
 }
