a5e72ba126235e01a0519789757a1aaa842884d3
3216004716 <875873332@qq.com>
2021-12-27 19:55:39 +0800

    添加新闻列表中的略缩图 调整了样式

--- src/components/loginDialog.vue
+++ src/components/loginDialog.vue
@@ -1,128 +1,197 @@
 <template>
-  <el-button type="primary" size="small" @click="dialogVisible = true">登录</el-button>
+  <el-button
+    size="small"
+    @click="dialogVisible = true"
+    style="
+      --el-button-bg-color: rgb(176, 190, 252);
+      --el-button-border-color: rgb(176, 190, 252);
+      --el-button-hover-bg-color: rgb(100, 126, 243);
+      --el-button-hover-border-color: rgb(100, 126, 243);
+      --el-button-active-bg-color: rgb(100, 126, 243);
+      --el-button-active-border-color: rgb(100, 126, 243);
+      color: white;
+    "
+    >登录</el-button
+  >
   <el-dialog
     v-model="dialogVisible"
     title="登录后内容更精彩"
     width="40%"
     :before-close="handleClose"
-  > 
-    <div class="box" :class="rotate ? 'flip' : ''"> 
-    <el-card shadow="never" class="login">
-      <el-row>
-        <el-col :span="14">
-        <el-form
-          :rules="rules"
-          ref="fromV"
-          :model="ruleForm"
-        >
-        <el-form-item prop="username">
-          <el-input
-            v-model="ruleForm.username"
-            placeholder="请输入邮箱地址"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            v-model="ruleForm.password"
-            placeholder="请输入密码"
-           type="password" 
-          ></el-input>
-        </el-form-item>
-        <el-form-item >
-          <el-checkbox label="是否同意《用户协议》和《隐私政策》" name="type"></el-checkbox>
-        </el-form-item>
-        <el-form-item size="large">
-          <el-button type="primary" style="width:100%">登录</el-button>
-        </el-form-item>
-        <div class="flex">
-          <el-link type="primary"  @click="rotate = !rotate">注册</el-link>
-          <el-link type="primary">忘记密码</el-link>
-        </div>
-      </el-form>
-        </el-col>
-        <el-col></el-col>
-      </el-row>
-    </el-card>
-    <el-card shadow="never" class="register">
-      <el-row>
-        <el-col :span="14">
-        <el-form
-          :rules="rules"
-          ref="fromV"
-          :model="ruleForm"
-        >
-        <el-form-item prop="username">
-          <el-input
-            v-model="ruleForm.username"
-            placeholder="请输入邮箱地址"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            v-model="ruleForm.password"
-            placeholder="请输入密码"
-           type="password" 
-          ></el-input>
-        </el-form-item>
-        <el-form-item >
-          <el-checkbox label="是否同意《用户协议》和《隐私政策》" name="type"></el-checkbox>
-        </el-form-item>
-        <el-form-item size="large">
-          <el-button type="primary" style="width:100%">注册</el-button>
-        </el-form-item>
-        <div class="flex">
-          <el-link type="primary" @click="rotate = !rotate">登录</el-link>
-          <el-link type="primary">忘记密码</el-link>
-        </div>
-      </el-form>
-        </el-col>
-        <el-col></el-col>
-      </el-row>
-    </el-card>
-  </div>
+  >
+    <div class="box" :class="rotate ? 'flip' : ''">
+      <el-card shadow="never" class="login">
+        <el-row>
+          <el-col :span="14">
+            <el-form :rules="rules" ref="fromV" :model="ruleForm">
+              <el-form-item prop="username">
+                <el-input
+                  v-model="ruleForm.username"
+                  placeholder="请输入邮箱地址"
+                ></el-input>
+              </el-form-item>
+              <el-form-item prop="password">
+                <el-input
+                  v-model="ruleForm.password"
+                  placeholder="请输入密码"
+                  type="password"
+                ></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-checkbox
+                  style="
+                    color: rgb(176, 190, 252);
+                    --el-checkbox-checked-text-color: rgb(100, 126, 243);
+                    --el-checkbox-checked-bg-color: rgb(100, 126, 243);
+                  "
+                  label="是否同意《用户协议》和《隐私政策》"
+                  name="type"
+                ></el-checkbox>
+              </el-form-item>
+              <el-form-item size="large">
+                <el-button
+                  type="primary"
+                  style="
+                    --el-button-bg-color: rgb(176, 190, 252);
+                    --el-button-border-color: rgb(176, 190, 252);
+                    --el-button-hover-bg-color: rgb(100, 126, 243);
+                    --el-button-hover-border-color: rgb(100, 126, 243);
+                    --el-button-active-bg-color: rgb(100, 126, 243);
+                    --el-button-active-border-color: rgb(100, 126, 243);
+                    width: 100%;
+                  "
+                  >登录</el-button
+                >
+              </el-form-item>
+              <div class="flex">
+                <el-link
+                  type="primary"
+                  @click="rotate = !rotate"
+                  style="color: rgb(100, 126, 243)"
+                  >注册</el-link
+                >
+                <el-link type="primary" style="color: rgb(100, 126, 243)"
+                  >忘记密码</el-link
+                >
+              </div>
+            </el-form>
+          </el-col>
+          <el-col></el-col>
+        </el-row>
+      </el-card>
+      <el-card shadow="never" class="register">
+        <el-row>
+          <el-col :span="14">
+            <el-form :rules="rules" ref="fromV" :model="ruleForm">
+              <el-form-item prop="username">
+                <el-input
+                  v-model="ruleForm.username"
+                  placeholder="请输入邮箱地址"
+                ></el-input>
+              </el-form-item>
+              <el-form-item prop="password">
+                <el-input
+                  v-model="ruleForm.password"
+                  placeholder="请输入密码"
+                  type="password"
+                ></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-checkbox
+                  style="
+                    color: rgb(176, 190, 252);
+                    --el-checkbox-checked-text-color: rgb(100, 126, 243);
+                    --el-checkbox-checked-bg-color: rgb(100, 126, 243);
+                  "
+                  label="是否同意《用户协议》和《隐私政策》"
+                  name="type"
+                ></el-checkbox>
+              </el-form-item>
+              <el-form-item size="large">
+                <el-button
+                  type="primary"
+                  style="
+                    --el-button-bg-color: rgb(176, 190, 252);
+                    --el-button-border-color: rgb(176, 190, 252);
+                    --el-button-hover-bg-color: rgb(100, 126, 243);
+                    --el-button-hover-border-color: rgb(100, 126, 243);
+                    --el-button-active-bg-color: rgb(100, 126, 243);
+                    --el-button-active-border-color: rgb(100, 126, 243);
+                    width: 100%;
+                  "
+                  >注册</el-button
+                >
+              </el-form-item>
+              <div class="flex">
+                <el-link
+                  type="primary"
+                  @click="rotate = !rotate"
+                  style="color: rgb(100, 126, 243)"
+                  >登录</el-link
+                >
+                <el-link type="primary" style="color: rgb(100, 126, 243)"
+                  >忘记密码</el-link
+                >
+              </div>
+            </el-form>
+          </el-col>
+          <el-col></el-col>
+        </el-row>
+      </el-card>
+    </div>
   </el-dialog>
 </template>
 
 <script>
 import { defineComponent, reactive, ref, toRefs } from "@vue/runtime-core";
-import { ElMessageBox } from 'element-plus'
+import { ElMessageBox } from "element-plus";
 // import MyInput from './input.vue'
 
 export default defineComponent({
-  name: 'loginDialog',
+  name: "loginDialog",
   // components: {
   //   MyInput
   // },
   setup() {
-    const dialogVisible = ref(false)
-    const rotate = ref(false)
-    let fromV = ref(null)
+    const dialogVisible = ref(false);
+    const rotate = ref(false);
+    let fromV = ref(null);
     const ruleForm = reactive({
-      username: '',
-      password: ''
-    })
-    // const username = ref('') 
-    // const password = ref('') 
+      username: "",
+      password: "",
+    });
+    // const username = ref('')
+    // const password = ref('')
 
     const rules = {
-      username: [{ required: true, message: '用户名不能为空', trigger: 'blur' },
-        { pattern: /^w+$/, message: '用户名只能由字母和数字组成', trigger: 'blur' }
+      username: [
+        { required: true, message: "用户名不能为空", trigger: "blur" },
+        {
+          pattern: /^w+$/,
+          message: "用户名只能由字母和数字组成",
+          trigger: "blur",
+        },
       ],
-      password: [{ required: true, message: '密码不能为空', trigger: 'blur' },
-        { min: 6, max: 15, message: '密码位数只能在6~15之间', trigger: 'blur' },
-        { pattern: /^[A-Za-z0-9][A-Z-z0-9_.]{5,14}$/, message: '密码由字母或数字开头，且只能为字母,数字,下划线及（.）', trigger: 'blur' }
-      ]
-    }
+      password: [
+        { required: true, message: "密码不能为空", trigger: "blur" },
+        { min: 6, max: 15, message: "密码位数只能在6~15之间", trigger: "blur" },
+        {
+          pattern: /^[A-Za-z0-9][A-Z-z0-9_.]{5,14}$/,
+          message: "密码由字母或数字开头，且只能为字母,数字,下划线及（.）",
+          trigger: "blur",
+        },
+      ],
+    };
     // 关闭弹窗
     const handleClose = (done) => {
-      ElMessageBox.confirm('Are you sure to close this dialog?')
+      ElMessageBox.confirm("Are you sure to close this dialog?")
         .then(() => {
-          done()
+          done();
         })
         .catch(() => {
           // catch error
-        })
-    }
+        });
+    };
 
     // const submitForm = async () => {
     //   const form = unref(fromV);
@@ -132,7 +201,7 @@ export default defineComponent({
     //     const { username, password } = ruleForm
     //     console.log(username, password)
     //   } catch (error) {
-    //   } 
+    //   }
     // }
 
     // submitForm()
@@ -144,45 +213,49 @@ export default defineComponent({
       // password,
       rules,
       handleClose,
-    }
-  }
-})
+    };
+  },
+});
 </script>
 
 <style lang="scss">
-a,a:link,a:visited,a:hover,a:active{
-    text-decoration: none;
-    color:inherit;
+a,
+a:link,
+a:visited,
+a:hover,
+a:active {
+  text-decoration: none;
+  color: inherit;
 }
 a:hover {
   text-decoration: underline;
-  color:gray;
+  color: grey;
 }
 
-.box{
+.box {
   height: 300px;
 }
 
-
-.flex{
+.flex {
   display: flex;
-  justify-content: space-around;;
+  justify-content: space-around;
 }
 .el-dialog__body {
   padding-top: 10px;
 }
-.el-dialog__title{
+.el-dialog__title {
   font-size: 24px;
 }
-.login{
+.login {
   transform: rotateY(0deg);
 }
 
-.register{
+.register {
   transform: rotateY(-180deg);
 }
 
-.login, .register{
+.login,
+.register {
   width: 94%;
   position: absolute;
   backface-visibility: hidden;
@@ -196,5 +269,4 @@ a:hover {
 .flip .register {
   transform: rotateY(0deg);
 }
- 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="bg">
     <div class="display-box">
-      <div class="header">
-        <LoginDialog></LoginDialog>
-      </div>
       <div class="banner">
         <div class="figure">
           <div class="banner-img"></div>
           <div class="shadow" />
         </div>
         <div class="title">
-          <h1>News Today</h1>
-          <p>时间</p>
+          <h1 class="frontpage_title">News Today</h1>
+          <p>
+            {{
+              new Date()
+                .toLocaleDateString()
+                .replace("/", "年")
+                .replace("/", "月") + "日"
+            }}
+          </p>
         </div>
       </div>
       <div class="container">
         <div class="nav-box">
           <NewsNavigator
-            v-for="(item, i) in newsItem.slice(0, 8)"
+            v-for="(item, i) in newsItems"
             :news="item"
             @click="selectNewsType(item)"
           />
@@ -25,7 +29,13 @@
         <div class="news-box">
           <router-view />
         </div>
+        <div class="bottom-line">
+          ———————————————— 到底啦！ ————————————————
+        </div>
       </div>
+    </div>
+    <div class="header">
+      <LoginDialog></LoginDialog>
     </div>
   </div>
 </template>
@@ -33,90 +43,96 @@
 <script>
 import NewsNavigator from "./NewsNavigator.vue";
 import NewsList from "./NewsList.vue";
-import LoginDialog from '../components/loginDialog.vue'
+import LoginDialog from "../components/loginDialog.vue";
 
 export default {
   name: "Home",
-  components: { NewsNavigator, NewsList, LoginDialog},
+  components: { NewsNavigator, NewsList, LoginDialog },
   data() {
+    const newsItems = [
+      { text: "推荐", active: true, icon: "all.png", tag: "__all__" },
+      { text: "热点", active: false, icon: "hot.png", tag: "news_hot" },
+      {
+        text: "社会",
+        active: false,
+        icon: "society.png",
+        tag: "news_society",
+      },
+      {
+        text: "娱乐",
+        active: false,
+        icon: "entertainment.png",
+        tag: "news_entertainment",
+      },
+      { text: "科技", active: false, icon: "tech.png", tag: "news_tech" },
+      {
+        text: "军事",
+        active: false,
+        icon: "military.png",
+        tag: "news_military",
+      },
+      { text: "体育", active: false, icon: "sports.png", tag: "news_sports" },
+      { text: "汽车", active: false, icon: "car.png", tag: "news_car" },
+      {
+        text: "财经",
+        active: false,
+        icon: "finance.png",
+        tag: "news_finance",
+      },
+      { text: "国际", active: false, icon: "world.png", tag: "news_world" },
+      {
+        text: "时尚",
+        active: false,
+        icon: "fashion.png",
+        tag: "news_fashion",
+      },
+      { text: "旅游", active: false, icon: "travel.png", tag: "news_travel" },
+      {
+        text: "探索",
+        active: false,
+        icon: "discovery.png",
+        tag: "news_discovery",
+      },
+      { text: "育儿", active: false, icon: "baby.png", tag: "news_baby" },
+      {
+        text: "养生",
+        active: false,
+        icon: "regimen.png",
+        tag: "news_regimen",
+      },
+      { text: "故事", active: false, icon: "story.png", tag: "news_story" },
+      { text: "美文", active: false, icon: "essay.png", tag: "news_essay" },
+      { text: "游戏", active: false, icon: "game.png", tag: "news_game" },
+      {
+        text: "历史",
+        active: false,
+        icon: "history.png",
+        tag: "news_history",
+      },
+      { text: "美食", active: false, icon: "food.png", tag: "news_food" },
+    ];
     return {
-      newsItem: [
-        { text: "推荐", active: true, icon: "all.png", tag: "__all__" },
-        { text: "热点", active: false, icon: "hot.png", tag: "news_hot" },
-        {
-          text: "社会",
-          active: false,
-          icon: "society.png",
-          tag: "news_society",
-        },
-        {
-          text: "娱乐",
-          active: false,
-          icon: "entertainment.png",
-          tag: "news_entertainment",
-        },
-        { text: "科技", active: false, icon: "tech.png", tag: "news_tech" },
-        {
-          text: "军事",
-          active: false,
-          icon: "military.png",
-          tag: "news_military",
-        },
-        { text: "体育", active: false, icon: "sports.png", tag: "news_sports" },
-        { text: "汽车", active: false, icon: "car.png", tag: "news_car" },
-        {
-          text: "财经",
-          active: false,
-          icon: "finance.png",
-          tag: "news_finance",
-        },
-        { text: "国际", active: false, icon: "world.png", tag: "news_world" },
-        {
-          text: "时尚",
-          active: false,
-          icon: "fashion.png",
-          tag: "news_fashion",
-        },
-        { text: "旅游", active: false, icon: "travel.png", tag: "news_travel" },
-        {
-          text: "探索",
-          active: false,
-          icon: "discovery.png",
-          tag: "news_discovery",
-        },
-        { text: "育儿", active: false, icon: "baby.png", tag: "news_baby" },
-        {
-          text: "养生",
-          active: false,
-          icon: "regimen.png",
-          tag: "news_regimen",
-        },
-        { text: "故事", active: false, icon: "story.png", tag: "news_story" },
-        { text: "美文", active: false, icon: "essay.png", tag: "news_essay" },
-        { text: "游戏", active: false, icon: "game.png", tag: "news_game" },
-        {
-          text: "历史",
-          active: false,
-          icon: "history.png",
-          tag: "news_history",
-        },
-        { text: "美食", active: false, icon: "food.png", tag: "news_food" },
-      ],
+      newsItems,
     };
   },
   methods: {
     selectNewsType(item) {
-      this.newsItem.forEach((item) => {
+      this.newsItems.forEach((item) => {
         item.active = false;
       });
       item.active = !item.active;
       this.$router.push({ name: "NewsList", params: { tag: item.tag } });
     },
   },
+  mounted() {
+    console.log("mounted", this);
+    this.$vuetify.theme.themes.light.primary = "red";
+  },
 };
 </script>
 
 <style scoped>
+@import url("https://fonts.font.im/css?family=Abril+Fatface");
 /* 背景 */
 #bg {
   width: 100%;
@@ -130,19 +146,22 @@ export default {
     rgba(205, 217, 252, 0.8) 100%
   );
 }
-
+.frontpage_title {
+  letter-spacing: 0.05em;
+  font-family: "Abril Fatface", cursive;
+}
 .display-box {
   width: 100%;
   display: flex;
+  position: relative;
   justify-content: center;
   flex-direction: column;
   align-items: center;
 }
-
 .header {
-  display: flex;
-  width: 100%;
-  justify-content: flex-end;
+  position: absolute;
+  right: 0px;
+  top: 0px;
   padding: 10px 10px 0 0;
 }
 .banner {
@@ -171,7 +190,7 @@ export default {
 @keyframes updown {
   0%,
   100% {
-    margin-top: 0px;
+    margin-top: 0rem;
   }
   50% {
     margin-top: -1rem;
@@ -202,9 +221,8 @@ export default {
 }
 
 .container {
-  width: 1100px;
+  width: 70%;
   min-height: 75vh;
-  padding: 10px 10px;
   background: rgb(255, 255, 255);
   background: rgb(255, 255, 255);
   background: linear-gradient(
@@ -212,21 +230,28 @@ export default {
     rgba(255, 255, 255, 0.8) 0%,
     rgba(255, 255, 255, 0.4) 100%
   );
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.4);
   display: flex;
   flex-direction: column;
-  /* justify-content: center; */
   align-items: center;
   border-radius: 30px 30px 0 0;
-  filter: blur();
 }
 .nav-box {
-  width: 180px;
-  height: 60px;
+  width: 100%;
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
 }
 .news-box {
-  margin-top: 20px;
-  width: 1000px;
+  width: 100%;
+}
+.bottom-line {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+  width: 100%;
+  text-align: center;
+  color: gray;
 }
 </style>
